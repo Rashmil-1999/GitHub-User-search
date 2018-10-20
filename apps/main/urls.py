b86efcbac949c9ca9a61d57ccc4290d69b497bba
Vikrant Gajria <vikrantgajria@gmail.com>
2018-10-20 11:01:06 +0530

    Added github client.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,11 +3,11 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-path('',views.index, name = 'index'),
-path('register/',views.register,name='register'),
-path('logout/',views.user_logout,name='logout'),
-path('special/',views.special,name='special'),
-path('user_login/',views.user_login,name='login'),
-path('search/',views.gh_user_search,name='gh_user_search'),
-path('search/<str:username>/<str:repo_name>/',views.gh_UserCommits,name='repo_detail'),
+    path('',views.index, name = 'index'),
+    path('register/',views.register,name='register'),
+    path('logout/',views.user_logout,name='logout'),
+    path('special/',views.special,name='special'),
+    path('user_login/',views.user_login,name='login'),
+    path('search/',views.gh_user_search,name='gh_user_search'),
+    path('search/<str:username>/<str:repo_name>/',views.gh_UserCommits,name='repo_detail'),
 ]
